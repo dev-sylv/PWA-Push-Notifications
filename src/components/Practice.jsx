cef91c38ddbbf64aed118adb34f85477ee3e33d8
nicsylvia <nicsylvia15f@gmail.com>
2023-05-22 00:46:01 -0700

    practicing push notification

---
+++ src/components/Practice.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Practice = () => {
+  return (
+    <div>
+<div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <div>Pratice</div>
+      <br />
+      <br />
+      <br />
+      <button style={{ padding: "10px 20px" }}>click</button>
+    </div>
+    </div>
+  )
+}
+
+export default Practice
