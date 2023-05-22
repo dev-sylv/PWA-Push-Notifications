65929540351d7eb06fcb5e870930bd3e943bd36d
nicsylvia <nicsylvia15f@gmail.com>
2023-05-22 01:20:51 -0700

    saved push updates

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React from "react";
+import notify from "react-push-notification";
+import pwa from "../assets/pwa.png";
 
 const Practice = () => {
+  const pushPractice = () => {
+    notify({
+      title: "PWA Testing",
+      icon: `${pwa}`,
+      message: "Push notification practice",
+      duration: 30000,
+    //   native: true
+    });
+    console.log("first") 
+  };
+
   return (
     <div>
-<div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-        flexDirection: "column",
-      }}
-    >
-      <div>Pratice</div>
-      <br />
-      <br />
-      <br />
-      <button style={{ padding: "10px 20px" }}>click</button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh",
+          flexDirection: "column",
+        }}>
+        <div>Pratice</div>
+        <br />
+        <br />
+        <br />
+        <button onClick={pushPractice} style={{ padding: "10px 20px", cursor: "pointer" }}>
+          click
+        </button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Practice
+export default Practice;
